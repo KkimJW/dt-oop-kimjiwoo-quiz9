9f339ef1c385111fb294a0fe9b878dd167c7391e
KimJiWoo <qweqwe2000@naver.com>
2017-11-09 12:50:05 +0900

    gitignore 추가 및 불필요 파일 repo에서 삭제

--- src/practice5/Solution.java
+++ src/practice5/Solution.java
@@ -1,7 +1,7 @@
 package practice5;
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
